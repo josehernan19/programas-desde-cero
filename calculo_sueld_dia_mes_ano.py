6d05e504ffa7e8517889ab9caaa11a56c3f96d67
Jose Hernandez <antonioaljorna@gmail.com>
2020-04-23 09:20:06 -0300

    calculando sueldos

---
+++ calculo_sueld_dia_mes_ano.py
@@ -0,0 +1,10 @@
+trabajo=int(input('Hola, queremos saber, cuantas horas trabajas? '))
+horas=int(input('cuanto cobras por hora? '))
+b = trabajo * horas
+c= 30
+d=365
+e= c * horas
+f= 365 * horas
+
+print('en un dia de trabajo ganas', b,'$', 'en un mes ganas ',e, 'en un ano ganas ', f)
+
