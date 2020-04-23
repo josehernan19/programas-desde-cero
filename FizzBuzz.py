cae1849bee98a9221a97eaccd23e4b690af2cd2c
Jose Hernandez <antonioaljorna@gmail.com>
2020-04-23 09:03:29 -0300

    Programa FizzBuzz

---
+++ FizzBuzz.py
@@ -0,0 +1,12 @@
+print("BIENVENIDO AL PROGRAMA FIZZBUZZ :) ")
+a=int(input("introduce un numero por favor "))
+b=int(input("introduzca otro numero "))
+for i in range(a,b) or range(b,a):
+    if i % 3 == 0 and i % 5 == 0:
+        print('FizzBuzz')
+    elif i % 5 == 0:
+        print('Buzz')
+    elif i % 3 == 0:
+        print('Fizz')
+    else:
+        print(i)
